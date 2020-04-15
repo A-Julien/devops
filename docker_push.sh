888941131db95c961cfed74c86fe901341a765b7
travis-ci <travis@travis-ci.org>
2020-04-16 00:16:31 +0200

    fix pushing issues

--- docker_push.sh
+++ docker_push.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_USERNAME}" --password-stdin
-
 docker build -t travis-ci-build-devops  gen_dockerfile/files
 docker images
 docker tag travis-ci-build-devops $DOCKER_USERNAME/travis-ci-build-devops
