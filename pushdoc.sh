8449cf8d366a5dd7ea789961c4e7bbea6bc472c4
Alaimo <julien.alaimo@gmail.com>
2020-04-15 11:15:59 +0200

    resolve path error

--- pushdoc.sh
+++ pushdoc.sh
@@ -4,12 +4,12 @@
 cd $HOME
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
-git clone --quiet --branch=gh-pages https://"${GH_TOKEN}"@github.com/ReadyTalk/swt-bling gh-pages > /dev/null
+git clone --quiet --branch=gh-pages https://"${GH_TOKEN}"@github.com/A-Julien/devops gh-pages > /dev/null
 
 # Commit and Push the Changes
 cd gh-pages
 git rm -rf ./javadoc
-cp -Rf target/site ./javadoc
+cp -Rf ./target/site ./javadoc
 git add -f .
 git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
 git push -fq origin gh-pages > /dev/null
