7f7eea68a282c043e018a05e7965551d7a876ac4
travis-ci <travis@travis-ci.org>
2020-04-15 22:51:38 +0200

    try to resolve path docker_push issues

--- docker_push.sh
+++ docker_push.sh
@@ -2,12 +2,14 @@
 
 ls
 ls dockerbuild
+ls dockerbuild/gen_dockerfile
+ls dockerbuild/gen_dockerfile/files
 
 #echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 #docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+echo $TRAVIS_BUILD_DIR
 
-cd dockerbuild/gen_dockerfile/files
-docker build -t travis-ci-build-devops .
+docker build -t travis-ci-build-devops  ${TRAVIS_BUILD_DIR}/dockerbuild/gen_dockerfile/files
 docker images
 docker tag travis-ci-build-devops $DOCKER_USERNAME/travis-ci-build-devops
 docker push $DOCKER_USERNAME/travis-ci-build-devops
