b2e7c721b56036529f0ce91135a0090137fbffca
travis-ci <travis@travis-ci.org>
2020-04-15 11:48:46 +0200

    resolve path error

--- pushdoc.sh
+++ pushdoc.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 # Get to the Travis build directory, configure git and clone the repo
+pwd
 cd $HOME
+ls -la
+pwd
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
-git clone --quiet --branch=gh-pages https://"${GH_TOKEN}"@github.com/A-Julien/devops gh-pages > /dev/null
+#git clone --quiet --branch=gh-pages https://"${GH_TOKEN}"@github.com/A-Julien/devops gh-pages > /dev/null
+#git clone --quiet --branch=gh-pages https://A-Julien/devops gh-pages > /dev/null
+git clone --quiet --branch=gh-pages https://"${GH_TOKEN}"@github.com:A-Julien/devops.git gh-pages > /dev/null
 
 # Commit and Push the Changes
 cd gh-pages
