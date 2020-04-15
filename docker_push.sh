c19637b4fd36f12fb9f14970feeacfb4a99aa8b3
Alaimo <julien.alaimo@gmail.com>
2020-04-15 09:59:50 +0200

    resolve interactive login from a non TTY device

--- docker_push.sh
+++ docker_push.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-docker build -t travis-ci-build-devops gen_dockerfile/files/Dockerfile
+ls
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker build -t travis-ci-build-devops gen_dockerfile/files/
 docker images
 docker tag travis-ci-build-devops $DOCKER_USERNAME/travis-ci-build-devops
 docker push $DOCKER_USERNAME/travis-ci-build-devops
