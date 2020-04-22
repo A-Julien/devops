ac6f3a1c7a07a9748a4d307eb3c7dc2c1b0ff6ac
travis-ci <travis@travis-ci.org>
2020-04-22 17:51:07 +0200

    test travis path

--- docker_push.sh
+++ docker_push.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-ls
-pwd
-docker build --build-arg PROJECT_VERSION=${PROJECT_VERSION} -t travis-ci-build-devops gen_dockerfile/files
+cd gen_dockerfile/files
+docker build --build-arg PROJECT_VERSION=${PROJECT_VERSION} -t travis-ci-build-devops . #gen_dockerfile/files
 docker images
 docker tag travis-ci-build-devops $DOCKER_USERNAME/travis-ci-build-devops
 docker push $DOCKER_USERNAME/travis-ci-build-devops
