2028057d685098123dba2b7effcde8781e58fdda
travis-ci <travis@travis-ci.org>
2020-04-27 15:18:39 +0200

    add tag to docker img

--- docker_push.sh
+++ docker_push.sh
@@ -9,5 +9,5 @@ ls
 echo "---------------------"
 docker build --build-arg PROJECT_VERSION=${PROJECT_VERSION} --build-arg HOME=${HOME}  -t travis-ci-build-devops . #gen_dockerfile/files
 docker images
-docker tag travis-ci-build-devops $DOCKER_USERNAME/travis-ci-build-devops
-docker push $DOCKER_USERNAME/travis-ci-build-devops
+docker tag travis-ci-build-devops $DOCKER_USERNAME/travis-ci-build-devops:${PROJECT_VERSION}
+docker push $DOCKER_USERNAME/travis-ci-build-devops:${PROJECT_VERSION}
